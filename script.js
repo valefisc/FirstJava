faadf0e059dbf110c84e5d17ddf92599adbd37bd
Valerie <valerie@valerie-fischer.de>
2022-02-01 01:17:07 +0100

    did the assignment

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstName = "Valerie";
+let lastName = "Fischer";
+let age = 18;
+alert ("My name is " + firstName + " " + lastName);
+alert ("I am " + age + " years old");
+console.log("This class is so much fun");
